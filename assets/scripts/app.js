1b44537877e10b99d2860f637c1a32ea2f7c2757
Ardiantohendy <bayuardianto421@gmail.com>
2025-04-28 22:57:16 +0700

    first commit (export & import in javascript)

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+// import { apiKey } from "./util.js";
+// import apiKey2 from "./util.js";
+import * as utils from "./util.js"; //grouping all variable into an object
+
+console.log("API_KEY: " + utils.apiKey);
+console.log("ADDRESS: " + utils.address);
+console.log("API_KEY(second): " + utils.default);
---
+++ assets/scripts/util.js
@@ -0,0 +1,4 @@
+export const apiKey = "234129fdj921kl1452ff23g2";
+export const address = "234129fdj921kl1452ff23g2"; //export more than 1 is allowed
+
+export default "234129fdj921kl1452ff23g2"; //export as default (1 per js file)
